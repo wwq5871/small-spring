e4a315b2308da7efe7acf69eec4e8d9cd11f86c6
wangwq <wangwq1@bsfit.com.cn>
2024-09-23 19:55:11 +0800

    删除

--- small-spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/HierarchicalBeanFactory.java
+++
@@ -1,9 +0,0 @@
-package cn.bugstack.springframework.beans.factory;
-
-/**
- * @description:
- * @author: wwq
- * @date: 2024/06/06/9:08
- */
-public interface HierarchicalBeanFactory extends BeanFactory {
-}
--- small-spring-step-07/src/main/java/cn/bugstack/springframework/beans/factory/HierarchicalBeanFactory.java
+++
@@ -1,9 +0,0 @@
-package cn.bugstack.springframework.beans.factory;
-
-/**
- * @description:
- * @author: wwq
- * @date: 2024/06/12/17:47
- */
-public interface HierarchicalBeanFactory extends BeanFactory{
-}
